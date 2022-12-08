681a5fd84751393829438e8a0ea3d388260ca22e
CarlosMatiz2 <FalcoMaster.-22>
2022-12-08 17:17:54 -0500

    eureka basic structure added

---
+++ eureka-service/src/test/java/co/com/poli/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
