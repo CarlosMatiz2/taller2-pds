1aa4fa9788a6217608662984b0d2771fe295ced5
CarlosMatiz2 <FalcoMaster.-22>
2022-12-08 12:44:18 -0500

    movies basic structure added

---
+++ movies-service/src/test/java/co/com/poli/moviesservice/MoviesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.moviesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
