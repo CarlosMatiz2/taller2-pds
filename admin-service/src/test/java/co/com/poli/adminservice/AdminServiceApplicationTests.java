b33ea1f6cdb93c9013e8dee01c8b4e67aabece83
CarlosMatiz2 <FalcoMaster.-22>
2022-12-10 19:37:21 -0500

    admin service added

---
+++ admin-service/src/test/java/co/com/poli/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
