6c02df725fb0c3a667e153d38e0506fc37f8582a
Sebastian Peralta <sebastian.peralta1@takeda.com>
2022-12-15 19:28:33 -0500

    feat: Imports test

--- users-service/src/test/java/co/com/poli/usersservice/UsersServiceApplicationTests.java
+++ users-service/src/test/java/co/com/poli/usersservice/UsersServiceApplicationTests.java
@@ -1,8 +1,20 @@
 package co.com.poli.usersservice;
 
+import co.com.poli.usersservice.clientFeign.BookingClient;
+import co.com.poli.usersservice.persistance.entity.User;
+import co.com.poli.usersservice.persistance.repository.UserRepository;
+import co.com.poli.usersservice.service.UserService;
+import co.com.poli.usersservice.service.UserServiceImpl;
+import com.google.inject.spi.PrivateElements;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
 @SpringBootTest
 class UsersServiceApplicationTests {
 
